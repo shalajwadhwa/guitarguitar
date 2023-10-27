38d33fcf216fde97c696e566ba8533ed8f6d1d9e
Karl McKenna <2562660m@student.gla.ac.uk>
2023-10-27 21:20:41 +0100

    Added app to list of installed apps

--- guitarguitar/guitarguitar/settings.py
+++ guitarguitar/guitarguitar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "team20"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,6 @@ ROOT_URLCONF = "guitarguitar.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
