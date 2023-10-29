002599188d1dc2a2c7e2987703a3ced55bbde297
Jackson Dam <2619114d@student.gla.ac.uk>
2023-10-29 10:13:23 +0000

    last

--- guitarguitar/team20/views.py
+++ guitarguitar/team20/views.py
@@ -6,10 +6,10 @@ from team20.models import Products
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.prompts.prompt import PromptTemplate
+# from langchain.chains.conversation.memory import ConversationBufferMemory
+# from langchain.chat_models import ChatOpenAI
+# from langchain.chains import ConversationChain
+# from langchain.prompts.prompt import PromptTemplate
 documents = SimpleDirectoryReader('data').load_data()
 index = VectorStoreIndex.from_documents(documents)
 
@@ -22,7 +22,7 @@ chat_engine = index.as_chat_engine(
         """You are a chatbot for guitarguitar.co.uk, and are very knowledgeable on guitars. You have information on their products. Sales prices are in GBP.
         Use QtyInStock to give stock information. Be careful to get the product's exact price and stock. DO NOT provide image links in your response.
 
-        You also have customer order information. Use can use 'Id' to get a specific order ID if requested, which you can then use to find who ordered the item, and what item it is. 
+        You also have customer order information. Use can use 'Id' to get a specific order ID if requested, which you can then use to find who ordered the item, and what item it is.
         Use CustomerId to find orders made by that customer and retrieve item details, if requested.
 
         You also have customer information, including ids, names, addresses, phone numbers and loyalty levels. You can return this information if requested by the user.
